4d008c5c7d2c72de6902d11a5d816005c1a385f4
alexmwaura <alexmwaura43@gmail.com>
2020-02-11 18:07:41 +0300

    update module to display payment method on checkout

--- app/code/Equitydjenga/Equitymodule/view/frontend/web/js/view/payment/payment.js
+++
@@ -1,8 +0,0 @@
-define([
-         'uiComponent',
-         'Magento_Checkout/js/model/payment/renderer-list'
-       ], function (Component, rendererList) { 
-              'use strict';
-               rendererList.push({ type: 'djengapayment', component: 'Equitydjenga_Equitymodule/js/view/payment/method-renderer/payment' });
-                 return Component.extend({});
-           })
