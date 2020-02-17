a90512ffe1e8c39172bc3d3bc24616ed774b2551
alexmwaura <alexmwaura43@gmail.com>
2020-02-17 19:51:12 +0300

    add variables for field values and include it on the knockout ajax object

--- app/code/Equity/Djenga/view/frontend/web/js/view/payment/method-renderer/equitydjengapayment-method.js
+++ app/code/Equity/Djenga/view/frontend/web/js/view/payment/method-renderer/equitydjengapayment-method.js
@@ -1,14 +1,25 @@
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Checkout/js/view/payment/default',
+
     ],
     function (Component) {
         'use strict';
+        var token = window.checkoutConfig.paymentToken
+        var total = window.checkoutConfig.quoteData.grand_total * 100
+        var ref = window.checkoutConfig.formKey
+        var name = window.checkoutConfig.customerData.email
 
         return Component.extend({
+
             defaults: {
-                template: 'Equity_Djenga/payment/equitydjengapayment'
+                template: 'Equity_Djenga/payment/equitydjengapayment',
+
             },
+            paymentToken: `${token}`,
+            orderTotal: `${total}`,
+            orderRef: `${ref}`,
+            custName: `${name}`,
 
             /** Returns send check to info */
             getMailingAddress: function() {
@@ -18,3 +29,5 @@ define(
         });
     }
 );
+
+
